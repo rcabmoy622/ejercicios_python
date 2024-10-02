ec71c44060bea60211df8a9d0bdeff1c98fb1392
Ruben <rcabmoy622@g.educaand.es>
2024-10-02 19:43:12 +0000

    Subo ejercicio_15

---
+++ ejercicio_15.py
@@ -0,0 +1,7 @@
+print("- - - Intercambiador de valores de variables numéricas - - -")
+A = int(input("Escribe un valor numérico para la variable A: "))
+B = int(input("Escribe un valor numérico para la variable B: "))
+t = A
+A = B
+B = t
+print("Intercambiamos y las variables quedan así: A =", A, ", B =", B)
