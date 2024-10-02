2792e0a6410a3e31b43cce0a192abc1b5589713d
Ruben <rcabmoy622@g.educaand.es>
2024-10-02 19:41:30 +0000

    Subo ejercicio_13

---
+++ ejercicio_13.py
@@ -0,0 +1,5 @@
+import math
+print("- - - Calculo de la raíz cuadrada y de la raíz cúbica de un número - - -")
+n = int(input("Escribe un número: "))
+print("La raíz cuadrada de ", n, " es: ",math.sqrt(n))
+print("La raíz cúbica de ", n, " es: ",n**(1/3)) # Elevar un número a 1/3 corresponde a hacer la raíz cúbica
