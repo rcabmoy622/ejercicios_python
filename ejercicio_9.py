644573af2ae65e994d603e94d33b36750ed4384c
Rubén <rcabmoy622@g.educaand.es>
2024-10-02 07:06:54 +0000

    Subo ejercicio_9

---
+++ ejercicio_9.py
@@ -0,0 +1,3 @@
+print("- - - Calcula un 15% de descuento en tu compra - - - ")
+c = int(input("Escribe el importe de tu compra (€): "))
+print("Con el 15% de descuento finalmente pagarás: ",c-(c*0.15)," €")
