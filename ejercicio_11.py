6f62e84030e6e3edcc2ac677428b0928756e67de
Rubén <rcabmoy622@g.educaand.es>
2024-10-02 09:02:47 +0000

    Subo ejercicio_11

---
+++ ejercicio_11.py
@@ -0,0 +1,4 @@
+print("- - - Calculo de la distancia de dos números - - -")
+n1 = int(input("Escribe el primer número: "))
+n2 = int(input("Escribe el segundo número: "))
+print("La distancia de ambos números es: ",abs(n1-n2))
